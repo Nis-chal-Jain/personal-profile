d8f4cb20093c2d6949525b239f5bac4ef4bcc3af
Nischal Jain <contactmenischal@gmail.com>
2024-10-15 17:50:47 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   return (
     <>
-      <div className="p-8 m-1  bg-[#ffffff]">
+      <div className="md:p-8 p-4 m-1  bg-[#ffffff]">
         <div className="flex gap-[5vw] flex-col md:flex-row bg-[#ffffff]">
-          <div className="rounded-xl bg-[#e2f6fc] hover:border  hover:border-blue-400 w-[85vw] md:w-[30%] h-[20vh] md:h-[90vh] flex flex-row md:flex-col">
+          <div className="rounded-xl bg-[#e2f6fc] hover:border ml-[-11px]  hover:border-blue-400 w-[95vw] md:w-[30%] h-[20vh] md:h-[90vh] flex flex-row md:flex-col">
             <img
               src={sherdil}
               alt=""
@@ -17,10 +17,10 @@ function App() {
             />
             <div className="flex flex-col justify-center items-center w-full mx-auto">
               <h5 className="text-3xl md:text-4xl poppins-semibold">Nischal Jain</h5>
-              <p className="md:mx-3 md:text-base text-sm mx-auto poppins-regular ">
-                Passionate about <strong>Core CS</strong> and <strong>building things</strong>
+              <p className="md:mx-3 md:text-base text-xs mx-auto poppins-regular ">
+                <span className="hidden md:inline">Passionate about</span> <strong>Core CS</strong> and <strong>building things</strong>
               </p>
-              <p className="poppins-regular md:text-base text-sm">Fullstack | Web | Mobile</p>
+              <p className="poppins-regular md:text-base text-xs">Fullstack | Web | Mobile</p>
               <div className="hidden md:block w-[80%] my-4">
                 <div className="bg-blue-500 h-[1px] w-full "></div>
                 <div className="m-auto my-4">
