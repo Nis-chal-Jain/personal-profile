a7515d809d1d9a7631a84ec502132034b35c68e0
Nischal Jain <89198650+Nis-chal-Jain@users.noreply.github.com>
2025-02-26 12:17:03 +0000

    changes in color scheme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,49 +30,49 @@ function Home() {
 
   return (
     <>
-      <div className="md:p-8 p-4 m-1  bg-[#ffffff]">
-        <div className="flex gap-[5vw] flex-col md:flex-row bg-[#ffffff]">
-          <div className="rounded-xl bg-blue-50 ml-[-11px] md:pt-6  w-[95vw] md:w-[30%] h-[20vh] md:h-[90vh] flex flex-row md:flex-col">
+      <div className="md:p-8 p-4 m-1  bg-[hsla(0,0%,0%,1)]">
+        <div className="flex gap-[5vw] flex-col md:flex-row bg-[hsla(0,0%,0%,1)] text-gray-50">
+          <div className="rounded-lg   bg-[hsla(0,0%,100%,0.09)]   bg-blue-50 ml-[-11px] md:pt-6  w-[95vw] md:w-[30%] h-[20vh] md:h-[90vh] flex flex-row md:flex-col">
             <img
               src={sherdil}
               alt=""
-              className="m-5 md:mx-auto md:w-[70%] md:h-auto max-h-[100%] rounded-lg "
+              className="m-5 md:mx-auto md:w-[70%] md:h-auto max-h-[100%] rounded-lg  -gray-700"
             />
             <div className="flex flex-col justify-center items-center w-full mx-auto space-y-2">
               <h5 className="text-3xl lg:text-4xl poppins-semibold">
                 Nischal Jain
               </h5>
-              <p className="lg:mx-3 lg:text-base text-xs mx-auto poppins-regular ">
+              <p className="lg:mx-3 lg:text-base text-xs mx-auto poppins-regular text-gray-300">
                 <span className="hidden lg:inline">Passionate about</span>{" "}
                 <strong>Core CS</strong> and <strong>building things</strong>
               </p>
-              <p className="poppins-regular md:text-base text-xs">
+              <p className="poppins-regular md:text-base text-xs text-gray-300">
                 Fullstack | Web | Mobile
               </p>
               <div className="hidden md:block space-y-6 w-[80%] my-4 ">
                 <div className="bg-blue-500 h-[1px] w-full "></div>
                 <div className="flex lg:flex-row lg:gap-0 flex-col">
-                  <p className="poppins-semibold inline">Email:</p>
-                  <p className="inline mx-2 lg:text-base text-xs">
+                  <p className="poppins-semibold inline text-gray-50">Email:</p>
+                  <p className="inline mx-2 lg:text-base text-xs text-gray-300">
                     contactmenischal@gmail.com
                   </p>
                 </div>
                 <div className="flex lg:flex-row flex-col">
-                  <p className="poppins-semibold inline">Location:</p>
-                  <p className="inline mx-2">Bangalore</p>
+                  <p className="poppins-semibold inline text-gray-50">Location:</p>
+                  <p className="inline mx-2 text-gray-300">Bangalore</p>
                 </div>
                 <div className="flex lg:flex-row flex-col">
-                  <p className="poppins-semibold inline">Ph no:</p>
-                  <p className="inline mx-2">+91 9380060740</p>
+                  <p className="poppins-semibold inline text-gray-50">Ph no:</p>
+                  <p className="inline mx-2 text-gray-300">+91 9380060740</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="rounded-xl bg-purple-200  ml-[-11px] w-[95vw] md:w-[70%] h-[90vh] md:h-[90vh] ">
+          <div className="rounded-lg   border border-gray-800   ml-[-11px] w-[95vw] md:w-[70%] h-[90vh] md:h-[90vh] ">
             <div className=" h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4 max-w-6xl mx-auto poppins-regular">
               <Link
                 to="/projects"
-                className="hover:bg-gray-100 bg-white rounded-lg p-3 flex flex-col md:text-4xl justify-evenly items-center text-lg min-h-[100px] sm:col-span-2 lg:col-span-2 cursor-pointer"
+                className=" bg-[hsla(0,0%,100%,0.09)]   rounded-lg p-3 flex flex-col md:text-4xl justify-evenly items-center text-lg min-h-[100px] sm:col-span-2 lg:col-span-2 cursor-pointer"
               >
                 Projects
                 <div className="flex gap-5 md:m-[-35px]">
@@ -81,16 +81,16 @@ function Home() {
                 </div>
               </Link>
 
-              <Link to="/edu" className="bg-white rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer hover:bg-gray-100">
+              <Link to="/edu" className="bg-[hsla(0,0%,100%,0.09)]   rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer ">
                 Education and Achievements
               </Link>
-              <Link to="/skills" className="bg-white rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer hover:bg-gray-100">
+              <Link to="/skills" className=" bg-[hsla(0,0%,100%,0.09)]   rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer ">
                 Skills
               </Link>
-              <Link to="/resume" className="bg-white rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer hover:bg-gray-100">
+              <Link to="/resume" className=" bg-[hsla(0,0%,100%,0.09)]   rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer ">
                 Resume
               </Link>
-              <Link to="/links" className="bg-white rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer hover:bg-gray-100">
+              <Link to="/links" className=" bg-[hsla(0,0%,100%,0.09)]   rounded-lg p-5 flex justify-center items-center text-lg min-h-[100px] cursor-pointer ">
                 Coding and Social profiles
               </Link>
             </div>
